7b5b9cf483e9a2d593adaacc871e73e35c69c6e8
masonbrad831 <masonbrad831@gmail.com>
2020-08-06 20:14:58 -0600

    Lets see if this works

--- src/prog/Main.java
+++ src/prog/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Testing the Connection");
         System.out.println("Test 2");
+        System.out.println("Mason's works??");
     }
 }
