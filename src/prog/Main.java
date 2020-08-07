e02f59873777f22257e262e50fb57d3d75de71c6
PhillipDuarte1 <pduarte@student.neumont.edu>
2020-08-06 20:21:38 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/prog/Main.java
+++ src/prog/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Testing the Connection");
         System.out.println("Test 2");
+<<<<<<< HEAD
+        System.out.println("Phillip Test");
+=======
+        System.out.println("Mason's works??");
+>>>>>>> origin/master
     }
 }
