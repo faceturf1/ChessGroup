6b6cc901d0da81feded88058a8ccd913481dd520
WinteryOS <54219355+WinteryOS@users.noreply.github.com>
2020-08-08 16:19:25 -0600

    Added Checkfile just need to implement chessboard

---
+++ src/prog/CheckFile.java
@@ -0,0 +1,30 @@
+package prog;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class CheckFile {
+    public static void Run() throws IOException {
+        checkExistence("c:/temp/temp.txt");
+    }
+
+
+    public static void checkExistence(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+
+        if (Files.exists(path)) {
+            System.out.println("Yes");
+        }
+
+        if (Files.notExists(path)) {
+            System.out.println("No");
+            List<String> lines = Arrays.asList("The first line", "The second line");
+            Path file = Paths.get("new-chess-game.txt");
+            Files.write(file, lines, StandardCharsets.UTF_8);
+        }
+    }
+}
