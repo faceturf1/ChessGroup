00dc76d6e49a6bbbb3e5a861fff9ff4c9f3146ce
WinteryOS <54219355+WinteryOS@users.noreply.github.com>
2020-08-08 16:18:43 -0600

    Added Checkfile just need to implement chessboard

--- src/prog/Main.java
+++ src/prog/Main.java
@@ -1,14 +1,18 @@
 package prog;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Testing the Connection");
-        System.out.println("Test 2");
-        System.out.println("Mason's works??");
-        System.out.print("Test 3");
-        System.out.println("Test Kolby joe");
-        System.out.println("Kia's Test");
-        System.out.println("Phill 2");
+    public static void main(String[] args) throws IOException {
+//        System.out.println("Testing the Connection");
+//        System.out.println("Test 2");
+//        System.out.println("Mason's works??");
+//        System.out.print("Test 3");
+//        System.out.println("Test Kolby joe");
+//        System.out.println("Kia's Test");
+//        System.out.println("Phill 2");
+
+        CheckFile.Run();
     }
 }
