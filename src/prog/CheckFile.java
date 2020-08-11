e9d99c70783b027119783089a100357f6d06a07c
that-writer-16 <64986807+that-writer-16@users.noreply.github.com>
2020-08-10 21:35:47 -0400

    I added save and load methods and a method to fix the board pieces.

--- src/prog/CheckFile.java
+++ src/prog/CheckFile.java
@@ -1,13 +1,16 @@
 package prog;
 
-import java.io.File;
-import java.io.IOException;
+import lib.ConsoleIO;
+
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.Arrays;
 import java.util.List;
 
 public class CheckFile {
+    private static ChessBoard chessBoard = new ChessBoard();
+
     public static void Run() throws IOException {
         checkExistence("c:/temp/temp.txt");
     }
@@ -25,6 +28,36 @@ public class CheckFile {
             List<String> lines = Arrays.asList("The first line", "The second line");
             Path file = Paths.get("new-chess-game.txt");
             Files.write(file, lines, StandardCharsets.UTF_8);
+        }
+    }
+
+    public void loadBoard(){
+        String fileName = ConsoleIO.promptForString("Enter the name of your saved board: ");
+        try{
+            FileInputStream fileInputStream = new FileInputStream(fileName + ".ser");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            chessBoard = (ChessBoard) objectInputStream.readObject();
+        } catch (ClassNotFoundException cnfe){
+            cnfe.printStackTrace();
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+    public void saveBoard(){
+        String fileName = ConsoleIO.promptForString("Enter a name to save your board under: ");
+        try{
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName + ".ser");
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        }catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+    public static void fixChessPieces(){
+        for(int i = 0; i > 8; i++) {
+            if (!chessBoard.piecePlacement(i, 3).equals("▭")) {
+                chessBoard = new ChessBoard();
+            }
         }
     }
 }
