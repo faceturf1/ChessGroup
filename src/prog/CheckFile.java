605866267ac1c39d6ebc44109e5f5a966cf46133
that-writer-16 <64986807+that-writer-16@users.noreply.github.com>
2020-08-11 22:05:42 -0400

    Deleted ConsolIO

--- src/prog/CheckFile.java
+++ src/prog/CheckFile.java
@@ -1,6 +1,5 @@
 package prog;
 
-import lib.ConsoleIO;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -32,7 +31,7 @@ public class CheckFile {
     }
 
     public void loadBoard(){
-        String fileName = ConsoleIO.promptForString("Enter the name of your saved board: ");
+        String fileName = promptForString("Enter the name of your saved board: ", false);
         try{
             FileInputStream fileInputStream = new FileInputStream(fileName + ".ser");
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
@@ -45,7 +44,7 @@ public class CheckFile {
         }
     }
     public void saveBoard(){
-        String fileName = ConsoleIO.promptForString("Enter a name to save your board under: ");
+        String fileName = promptForString("Enter a name to save your board under: ", false);
         try{
             FileOutputStream fileOutputStream = new FileOutputStream(fileName + ".ser");
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
@@ -60,4 +59,36 @@ public class CheckFile {
             }
         }
     }
+
+    public static String promptForString(String prompt, boolean allowBlank) {
+        if (prompt == null || prompt.isBlank()) {
+            throw new IllegalArgumentException("The prompt cannot be null, empty or just whitespece.");
+        }
+        BufferedReader buffy = new BufferedReader(new InputStreamReader(System.in));
+        String input = null;
+        boolean isInvalid = true;
+
+        do {
+            System.out.print(prompt);
+
+            try {
+                input = buffy.readLine();
+                isInvalid = input == null;
+
+                if (!allowBlank) {
+                    isInvalid = input == null || (!allowBlank && input.isBlank());
+                }
+
+                if (isInvalid) {
+                    System.out.println("Your input cannot be null" + (!allowBlank ? " empty, or just white space" : " ") + ". Please, try again.");
+                }
+            } catch (IOException ioe) {
+                System.out.println("There was a technical issue. Please, try again.");
+            }
+
+        } while (isInvalid);
+
+        return input;
+    }
+
 }
