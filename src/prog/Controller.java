0c5f4a8fa7348e18c6ea3ddc2b72553bee38410d
PhillipDuarte1 <pduarte@student.neumont.edu>
2020-08-16 16:50:07 -0600

    Simple taking turns method which can have colors added to players, Remember first player starts off as white

--- src/prog/Controller.java
+++ src/prog/Controller.java
@@ -11,11 +11,13 @@ public class Controller {
             if (firstPlayer /*== white type */) {
                 System.out.println("Player 1's Turn:");
                 /*Make first player to black*/
-                input.nextLine();
+                firstPlayer = false;
+                input.next();
             } else {
                 System.out.println("Player 2's Turn:");
                 /*can say firstplayer is white*/
-                input.nextLine();
+                firstPlayer = true;
+                input.next();
             }
         }
     }
@@ -23,4 +25,11 @@ public class Controller {
     public boolean getCurrentPlayerTurn() {
         return firstPlayer;
     }
+
+   /* private void inputChecker() {
+        String invalid = input.next();
+        if(invalid.equals(" ") || invalid.equals(null)) {
+            throw new IllegalArgumentException("Input cannot be empty or invalid, please try again.");
+        }
+    }*/
 }
